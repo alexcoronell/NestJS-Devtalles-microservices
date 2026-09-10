58750a1f7dccd5316219d6aeeaa401ed51ef8931
Alex Coronell <alexcoronell@gmail.com>
2026-09-10 17:32:03 -0500

    Product converted to microservice

--- products-ms/src/main.ts
+++ products-ms/src/main.ts
@@ -3,14 +3,26 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { envs } from './config/envs';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const logger = new Logger('Product Microservice');
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        port: envs.PORT,
+      },
+    },
+  );
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
   );
-  await app.listen(envs.PORT);
-  logger.log(`Application is running on: ${envs.PORT}`);
+  await app.listen();
+
+  // await app.startAllMicroservices(); // Hybrid option
+
+  logger.log(`Products Microservice is running on: ${envs.PORT}`);
 }
 bootstrap();
